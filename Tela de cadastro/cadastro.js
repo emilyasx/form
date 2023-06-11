7c4549cc24e0484e6b3f431e69de66ceccd6b225
Emily Yasmin <yasminemily225@gmail.com>
2023-06-11 20:07:13 -0300

    commit atualizado

---
+++ Tela de cadastro/cadastro.js
@@ -0,0 +1,34 @@
+class Validator {
+    constructor(){
+        this,validations =  [
+
+        ]
+    }
+}
+
+//iniciara validação de todos os campos 
+
+validate(form) 
+    // pegar inputs
+    let inputs = form.getElementsByTagName('input')
+
+    // html colection para array
+
+    let inputsArray = 
+
+
+let form = document.getElementById("register-form");
+let submit = document.getElementById("btn-submit"); 
+
+let validator = new validator ();
+
+//evento que dispara as validações 
+
+Submit.addEventListener('click' , function(e) { 
+
+e.preventDefault();
+
+validator.validate(form);
+
+});
+
